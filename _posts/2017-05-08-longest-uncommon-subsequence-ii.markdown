e2475b540545f764290b008506b418fdc83fa613
jason <johnny.lyy@gmail.com>
2017-05-08 21:25:57 -0400

    add leetcode dir

---
+++ _posts/2017-05-08-longest-uncommon-subsequence-ii.markdown
@@ -0,0 +1,32 @@
+---
+layout: post
+title: longest uncommon subsequence ii
+date: 2017-05-08
+tags:
+- Algorithm
+categories:
+- String
+author: Jason
+---
+**Given a list of strings, you need to find the longest uncommon subsequence among them. The longest uncommon subsequence is defined as the longest subsequence of one of these strings and this subsequence should not be any subsequence of the other strings. A subsequence is a sequence that can be derived from one sequence by deleting some characters without changing the order of the remaining elements. Trivially, any string is a subsequence of itself and an empty string is a subsequence of any string. The input will be a list of strings, and the output needs to be the length of the longest uncommon subsequence. If the longest uncommon subsequence doesn't exist, return -1.**
+
+```python
+class Solution(object):
+    def findLUSlength(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: int
+        """
+        strs.sort(key=len, reverse=True)
+        for i, word1 in enumerate(strs):
+            if all(not self.is_subseq(word1, word2) for j, word2 in enumerate(strs) if i != j):
+                return len(word1)
+        return -1
+
+    def is_subseq(self, word1, word2):
+        i = 0
+        for char in word2:
+            if i < len(word1) and word1[i] == char:
+                i += 1
+        return i == len(word1)
+```
--- _utils/generate_post.py
+++ _utils/generate_post.py
@@ -4,7 +4,7 @@ import datetime as dt
 import sys
 import argparse
 
-def generate_post(title):
+def generate_post(input_file):
     """Description
     generate a post
     @param param:  Description
@@ -15,15 +15,21 @@ def generate_post(title):
 
     @raise e:  Description
     """
+
     today_date = dt.datetime.today().strftime("%Y-%m-%d")
-    filename = '_posts/' + '-'.join([today_date, title.replace(' ', '-')]) + ".markdown"
-    post = ['---', 'layout: post', 'title: {0}'.format(title), 'date: {0}'.format(today_date), 'tags:', '- ', 'categories:', '- ' , "author: Jason", '---', '**title**']
-    with open (filename, 'w') as output:
+    problem_name = input_file.split(".")[1]
+    title = problem_name.replace("-", " ")
+    output_file = '-'.join([today_date, problem_name]) + ".markdown"
+    post = ['---', 'layout: post', 'title: {0}'.format(title), 'date: {0}'.format(today_date), 'tags:', '- Algorithm', 'categories:', '- ' , "author: Jason", '---', '**title**']
+    with open (input_file, "r") as input:
+        lines = input.read()
+    with open (output_file, 'w') as output:
         output.write('\n'.join(post))
+        output.write('\n\n```python\n' + lines + '```')
 
 if __name__ == "__main__":
     parse = argparse.ArgumentParser(description='add new post')
-    parse.add_argument('title', help="post title as a string")
+    parse.add_argument('filename', help="input filename")
     args = parse.parse_args()
 
-    generate_post(args.title)
+    generate_post(args.filename)
