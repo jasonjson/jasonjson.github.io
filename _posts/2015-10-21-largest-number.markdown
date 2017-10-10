480023096d38db2b25de890185daf8af870bffd6
jason <johnny.lyy@gmail.com>
2017-10-09 21:46:24 -0400

    update

--- _posts/2015-10-21-largest-number.markdown
+++ _posts/2015-10-21-largest-number.markdown
@@ -1,15 +1,14 @@
 ---
 layout: post
-title: Largest Number
+title: 179 - Largest Number
 date: 2015-10-21 14:24:22.000000000 -04:00
 tags:
 - Leetcode
 categories:
 - Brain Teaser
-- Integer
 author: Jason
 ---
-<p><strong><em>Given a list of non negative integers, arrange them such that they form the largest number.</em></strong></p>
+**Given a list of non negative integers, arrange them such that they form the largest number.**
 
 
 ``` java
@@ -21,11 +20,11 @@ public class Solution {
     public String largestNumber(int[] num) {
         // write your code here
         if (num == null || num.length == 0) return "";
-        
+
         List<String> list = new ArrayList<String>();
         for (int n : num) {
             list.add(String.valueOf(n));
-        }        
+        }
         Collections.sort(list, new Comparator<String>() {
             public int compare (String a, String b) {
                 return (b + a).compareTo(a + b);
@@ -42,4 +41,15 @@ public class Solution {
         }
     }
 }
+```
+
+``` python
+class Solution:
+    # @param {integer[]} nums
+    # @return {string}
+    def largestNumber(self, nums):
+
+        nums = [str(num) for num in nums]
+        nums.sort(cmp = lambda x, y : cmp(y + x, x + y))
+        return "".join(nums).lstrip("0") or "0"
 ```
--- _posts/test.py
+++ _posts/test.py
@@ -1,42 +1,25 @@
 #!/usr/bin/python
-class Solution(object):
-    def fractionToDecimal(self, numerator, denominator):
-        """
-        :type numerator: int
-        :type denominator: int
-        :rtype: str
-        """
 
-        if not numerator or not denominator:
+class Solution:
+    # @param {integer[]} nums
+    # @return {string}
+    def largestNumber(self, nums):
+        if not nums:
             return ""
+        nums.sort(self.comparator)
+        import pprint
+        pprint.pprint(nums)
+        return "".join([str(num) for num in nums])
 
-        ret = []
-        nume, deno = abs(numerator), abs(denominator)
-        if numerator > 0 == denominator < 0:
-            ret.append("-")
-        ret.append(str(nume / deno))
-        nume %= deno
-        if nume == 0:
-            return "".join(ret)
+    def comparator(self, a, b):
+        if str(a) + str(b) < str(b) + str(a):
+            return 1
+        elif str(a) + str(b) > str(b) + str(a):
+            return -1
         else:
-            ret.append(".")
-        repeating_map = {}
-        index = 1
-        while nume:
-            print nume
-            nume *= 10
-            digit = nume / deno
-            if nume not in repeating_map:
-                ret.append(str(digit))
-                repeating_map[nume] = index
-                index += 1
-            else:
-                ret.insert(ret.index(".") + repeating_map[nume], "(")
-                ret.append(")")
-                break
-            nume %= deno
-        return "".join(ret)
+            return 0
+
 if __name__ == "__main__":
     solution = Solution()
     import pprint
-    pprint.pprint(solution.fractionToDecimal(1, 6))
+    pprint.pprint(solution.largestNumber([3, 30, 34, 5, 9]))
