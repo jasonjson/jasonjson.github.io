6bfa31835f3ca673f0a272a9ab792ab2e3714886
jason <johnny.lyy@gmail.com>
2017-10-22 14:48:30 -0400

    update

--- _posts/2015-10-21-ugly-number.markdown
+++ _posts/2015-10-21-ugly-number.markdown
@@ -1,34 +1,50 @@
 ---
 layout: post
-title: Ugly Number
+title: 263 - Ugly Number
 date: 2015-10-21 02:41:36.000000000 -04:00
 tags:
 - Leetcode
 categories:
-- Integer
+- Brain Teaser
 author: Jason
 ---
-<p><strong><em>Write a program to check whether a given number is an ugly number. Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7. Note that 1 is typically treated as an ugly number.</em></strong></p>
+**Write a program to check whether a given number is an ugly number. Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7. Note that 1 is typically treated as an ugly number.**
 
 
 ``` java
 public class Solution {
     public boolean isUgly(int num) {
         if (num <= 0) return false;
-        if (num == 1) return true;
-        
+
         while (num % 5 == 0) {
             num = num / 5;
         }
-        
         while (num % 3 == 0) {
             num = num / 3;
         }
-        
         while (num % 2 == 0) {
             num = num / 2;
         }
         return num == 1;
     }
 }
+```
+
+``` python
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+
+        while num % 5 == 0:
+            num /= 5
+        while num % 3 == 0:
+            num /= 3
+        while num % 2 == 0:
+            num /= 2
+        return num == 1
 ```
--- _posts/test.py
+++ _posts/test.py
@@ -1,52 +1,26 @@
 #!/usr/bin/python
 
 class Solution(object):
-    def diffWaysToCompute(self, input):
+    def nthUglyNumber(self, n):
         """
-        :type input: str
-        :rtype: List[int]
+        :type n: int
+        :rtype: int
         """
 
-        if not input:
-            return []
-
-        num_op = self.get_num_op(input)
-        print num_op
-        return self.helper(num_op, 0, len(num_op) - 1)
-
-    def helper(self, num_op, start, end):
-        if start == end:
-            return [num_op[start]]
-        ret = []
-        for i in xrange(start + 1, end, 2):
-            operator = num_op[i]
-            left_ret = self.helper(num_op, start, i - 1)
-            right_ret = self.helper(num_op, i + 1, end)
-            for left in left_ret:
-                for right in right_ret:
-                    if operator == "+":
-                        ret.append(left + right)
-                    elif operator == "-":
-                        ret.append(left - right)
-                    elif operator == "*":
-                        ret.append(left * right)
-        return ret
-
-    def get_num_op(self, input):
-        ret = []
-        index = 0
-        num = 0
-        while index < len(input):
-            if input[index].isdigit():
-                num = num * 10 + int(input[index])
-            else:
-                ret.append(num)
-                ret.append(input[index])
-                num = 0
-            index += 1
-        ret.append(num)
-        return ret
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        index2 = index3 = index5 = 1
+        for i in xrange(2, n + 1):
+            dp[i] = min(dp[index2] * 2, dp[index3] * 3, dp[index5] * 5)
+            if dp[i] == dp[index2] * 2:
+                index2 += 1
+            if dp[i] == dp[index3] * 3:
+                index3 += 1
+            if dp[i] == dp[index5] * 5:
+                index5 += 1
+        print dp
+        return dp[n]
 if __name__ == "__main__":
     solution = Solution()
     import pprint
-    pprint.pprint(solution.diffWaysToCompute("2-1-1"))
+    pprint.pprint(solution.nthUglyNumber(1))
