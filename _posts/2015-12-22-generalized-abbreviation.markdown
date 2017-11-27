4a94ff8881e322ab95145da7857751cf50a2b297
jason <johnny.lyy@gmail.com>
2017-11-26 21:28:51 -0500

    update

--- _posts/2015-12-22-generalized-abbreviation.markdown
+++ _posts/2015-12-22-generalized-abbreviation.markdown
@@ -1,15 +1,15 @@
 ---
 layout: post
-title: Generalized Abbreviation
+title: 320 - Generalized Abbreviation
 date: 2015-12-22 16:04:37.000000000 -05:00
 tags:
 - Leetcode
 categories:
-- Brain Teaser
 - DFS Backtracking
 author: Jason
 ---
 **Write a function to generate the generalized abbreviations of a word. Example: Given word = "word", return the following list (order does not matter): ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]**
+
 
 ``` java
 public class Solution {
--- _posts/test.py
+++ _posts/test.py
@@ -1,16 +1,26 @@
-import random
 class Solution(object):
-    def pickRandom(self, root):
-        if not root:
-            return
+    def generateAbbreviations(self, word):
+        """
+        :type word: str
+        :rtype: List[str]
+        """
+        if not word:
+            return []
 
-        stack, ret = [], []
-        while stack or root:
-            if root:
-                stack.append(root)
-                root = root.left
-            else:
-                root = stack.pop()
-                ret.append(root)
-                root = root.right
-        return ret[random.randInt(0, len(ret) - 1)]
+        ret = []
+        self.helper(word, 0, [], ret, False)
+        return ret
+
+    def helper(self, word, start, curr, ret, add_number):
+        if start == len(word):
+            ret.append("".join(curr))
+            return
+        if not add_number:
+            for i in xrange(start + 1, len(word) + 1):
+                number = str(i - start)
+                self.helper(word, i, curr + [number], ret, True)
+        self.helper(word, start + 1, curr + [word[start]], ret, False)
+if __name__ == "__main__":
+    solution = Solution()
+    import pprint
+    pprint.pprint(solution.generateAbbreviations("word"))
