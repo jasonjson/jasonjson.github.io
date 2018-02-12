36b60b336edacc5d6f6bdc72d35e39b144aea424
jason <johnny.lyy@gmail.com>
2018-02-11 21:07:47 -0500

    update

--- _posts/2015-10-21-trapping-rain-water.markdown
+++ _posts/2015-10-21-trapping-rain-water.markdown
@@ -1,16 +1,15 @@
 ---
 layout: post
-title: Trapping Rain Water
+title: 42 - Trapping Rain Water
 date: 2015-10-21 14:41:08.000000000 -04:00
 tags:
 - Leetcode
 categories:
-- Brain Teaser
 - Dynamic Programming
 author: Jason
 ---
-<p><strong><em>Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.</em></strong></p>
-
+**Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining**.
+[reference](http://blog.csdn.net/linhuanmars/article/details/20888505)
 
 ``` java
 public class Solution {
@@ -21,7 +20,7 @@ public class Solution {
     public int trapRainWater(int[] heights) {
         // write your code here
         if (heights == null || heights.length == 0) return 0;
-        
+
         int[] container = new int[heights.length];
         int max = 0, result = 0;
         for (int i = 0; i < heights.length; i++) {
@@ -37,7 +36,34 @@ public class Solution {
             result +=  diff > 0 ? diff : 0;
         }
         return result;
-    }//http://blog.csdn.net/linhuanmars/article/details/20888505
+    }
 }
+```
 
+``` python
+class Solution:
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+        if not height:
+            return 0
+
+        container = [0] * len(height)
+        max_height = ret = 0
+
+        for i in range(len(height)):
+            container[i] = max_height
+            max_height = max(max_height, height[i])
+
+        max_height = 0
+        for i in reversed(range(len(height))):
+            container[i] = min(container[i], max_height)
+            max_height = max(max_height, height[i])
+            diff = container[i] - height[i]
+            ret += diff if diff > 0 else 0
+
+        return ret
 ```
--- _posts/test.py
+++ _posts/test.py
@@ -1,41 +1,31 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-class Solution(object):
-    def solveSudoku(self, board):
+class Solution:
+    def trap(self, height):
         """
-        :type board: List[List[str]]
-        :rtype: void Do not return anything, modify board in-place instead.
+        :type height: List[int]
+        :rtype: int
         """
-        if not board:
-            return
 
-        self.helper(0, board)
+        if not height:
+            return 0
 
-    def helper(self, start, board):
-        if start == 81:
-            return True
+        container = [0] * len(height)
+        max_height = ret = 0
 
-        i, j = start / 9, start % 9
-        if board[i][j] != ".":
-            return self.helper(start + 1, board)
-        else:
-            for candidate in xrange(1, 10):
-                if self.is_valid(board, i, j, str(candidate)):
-                    board[i][j] = str(candidate)
-                    if self.helper(start + 1, board):
-                        return True
-                    board[i][j] = "."
-            return False
+        for i in range(len(height)):
+            __import__('pdb').set_trace()
+            container[i] = max_height
+            max_height = max(max_height, height[i])
 
-    def is_valid(self, board, i, j , candidate):
-        print i, j, candidate
-        for k in xrange(9):
-            if board[i][k] == candidate or board[k][j] == candidate or board[i - i % 3 + k /3 ][j - j %3 + k % 3] == candidate:
-                return False
-        return True
+        max_height = 0
+        for i in reversed(range(len(height))):
+            container[i] = min(container[i], max_height)
+            max_height = max(max_height, height[i])
+            diff = container[i] - height[i]
+            ret += diff if diff > 0 else 0
+
+        return ret
+
 if __name__ == "__main__":
     solution = Solution()
-    board = [[".",".","9","7","4","8",".",".","."],["7",".",".",".",".",".",".",".","."],[".","2",".","1",".","9",".",".","."],[".",".","7",".",".",".","2","4","."],[".","6","4",".","1",".","5","9","."],[".","9","8",".",".",".","3",".","."],[".",".",".","8",".","3",".","2","."],[".",".",".",".",".",".",".",".","6"],[".",".",".","2","7","5","9",".","."]]
-    solution.solveSudoku(board)
-    __import__('pprint').pprint(board)
+    import pprint
+    pprint.pprint(solution.trap([0]))
