fc10ee19c34e412a2e41f92479fc677f5e60ca14
jason <johnny.lyy@gmail.com>
2018-02-28 14:01:31 -0500

    update

---
+++ _posts/2018-02-28-sentence-similarity-ii.markdown
@@ -0,0 +1,80 @@
+---
+layout: post
+title: 737 - Sentence Similarity II
+date: 2018-02-28
+tags:
+- Leetcode
+categories:
+- String
+author: Jason
+---
+**Given two sentences words1, words2 (each represented as an array of strings), and a list of similar word pairs pairs, determine if two sentences are similar.**
+
+
+```python
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        if len(words1) != len(words2):
+            return False
+
+        word_map = collections.defaultdict(set)
+        for a, b in pairs:
+            word_map[a].add(b)
+            word_map[b].add(a)
+
+        for w1, w2 in zip(words1, words2):
+            visited = set([w1])
+            if not self.verify(w1, w2, word_map, visited):
+                return False
+        return True
+
+
+    def verify(self, w1, w2, w_map, visited):
+        if w1 == w2:
+            return True
+        for word in w_map[w1]:
+            if word not in visited:
+                visited.add(word)
+                if self.verify(word, w2, w_map, visited):
+                    return True
+                visited.discard(word)
+        return False
+```
+
+``` python
+class Solution(object):
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
+        """
+        :type words1: List[str]
+        :type words2: List[str]
+        :type pairs: List[List[str]]
+        :rtype: bool
+        """
+
+        if len(words1) != len(words2):
+            return False
+        union = {}
+        for (a, b) in pairs:
+            p1, p2 = self.find(a, union), self.find(b, union)
+            union[p1] = p2
+
+        for w1, w2 in zip(words1, words2):
+            if self.find(w1, union) != self.find(w2, union):
+                return False
+        return True
+
+
+    def find(self, s, union):
+        if s == union.get(s, s):
+            union[s] = s
+            return s
+        else:
+            return self.find(union[s], union)
+
+```
--- _posts/test.py
+++ _posts/test.py
@@ -1,29 +1,44 @@
+import collections
 class Solution(object):
-    def decodeString(self, s):
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
         """
-        :type s: str
-        :rtype: str
+        :type words1: List[str]
+        :type words2: List[str]
+        :type pairs: List[List[str]]
+        :rtype: bool
         """
 
-        if not s:
-            return ""
+        if len(words1) != len(words2):
+            return False
 
-        stack = [["", 1]]
-        num = 0
-        for char in s:
-            if char.isdigit():
-                num = num * 10 + int(char)
-            elif char == "[":
-                stack.append(["", num])
-                num = 0
-            elif char == "]":
-                new_s, count = stack.pop()
-                stack[-1][0] += new_s * count
-            else:
-                stack[-1][0] += char
-        new_s, count = stack.pop()
-        return new_s * count
+        word_map = collections.defaultdict(set)
+        # for word in words1:
+            # word_map[word].add(word)
+        # for word in words2:
+            # word_map[word].add(word)
+        for a, b in pairs:
+            word_map[a].add(b)
+            word_map[b].add(a)
+
+        for w1, w2 in zip(words1, words2):
+            visited = set([w1])
+            if not self.verify(w1, w2, word_map, visited):
+                return False
+        return True
+
+
+    def verify(self, w1, w2, w_map, visited):
+        if w1 == w2:
+            return True
+        for word in w_map[w1]:
+            if word not in visited:
+                visited.add(word)
+                if self.verify(word, w2, w_map, visited):
+                    return True
+                visited.discard(word)
+        return False
+
 if __name__ == "__main__":
     solution = Solution()
     import pprint
-    pprint.pprint(solution.decodeString("2[abc]3[cd]ef"))
+    pprint.pprint(solution.areSentencesSimilarTwo(["great", "acting", "skills"], ["fine", "drama", "talent"], [["great", "good"], ["fine", "good"], ["acting","drama"], ["skills","talent"]]))
